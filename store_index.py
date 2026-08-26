fa4a96ab7dcd5bc7f90531862a6c69976e42498b
vishal kumar <149944501+VishalKumar-12@users.noreply.github.com>
2026-08-27 01:41:44 +0530

    Change index dimension from 1024 to 384

--- store_index.py
+++ store_index.py
@@ -34,7 +34,8 @@ index_name = "healthbot-multilingual"
 if not pc.has_index(index_name):
     pc.create_index(
         name=index_name,
-        dimension=1024,
+        dimension=384,
+        # dimension=1024,
         # dimension=384,
         metric="cosine",
         spec=ServerlessSpec(
